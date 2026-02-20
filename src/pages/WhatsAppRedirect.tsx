b51ff1b8831d84b2e7f99cc59884a4a4afc59d52
Sakticahyaa <sakticahyabuana@mail.ugm.ac.id>
2026-02-20 14:48:02 +0700

    Move Google tag to index.html for Tag Assistant compatibility

--- src/pages/WhatsAppRedirect.tsx
+++ src/pages/WhatsAppRedirect.tsx
@@ -2,22 +2,6 @@ import { useEffect } from 'react';
 
 function WhatsAppRedirect() {
   useEffect(() => {
-    // Load Google tag
-    const script1 = document.createElement('script');
-    script1.async = true;
-    script1.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17951489596';
-    document.head.appendChild(script1);
-
-    const script2 = document.createElement('script');
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-17951489596');
-    `;
-    document.head.appendChild(script2);
-
-    // Redirect to WhatsApp
     window.location.href = 'https://wa.me/628119155850';
   }, []);
 
